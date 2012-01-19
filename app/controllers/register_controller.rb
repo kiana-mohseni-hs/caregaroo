2e14de6b944a8ecbfccdf14609976c2e5d3ecde2
Michael Wu <mwu@caregaroo.com>
2012-01-18 23:14:53 -0800

    add missing files

---
+++ app/controllers/register_controller.rb
@@ -0,0 +1,60 @@
+class RegisterController < ApplicationController
+  respond_to :html, :js, :json
+    
+  def signup
+     @pilot_signup = PilotSignup.new(params[:pilot_signup])
+     @pilot_signup.save
+     if @pilot_signup.signup_type == 'ebook'
+       cookies[:ebook] = "1"
+     elsif @pilot_signup.signup_type == 'pilot'
+       cookies[:pilot] = "1"
+     end
+     respond_with( @pilot_signup, :layout => false )     
+  end
+  
+  def index
+    logger.debug "(index) #{params[:invitation_token]}"
+      
+    session[:signup_params] ||= {}
+    @user = User.new(session[:signup_params])
+    @user.current_step = session[:signup_step]
+    
+    @invitations = Invitation.find_by_token(params[:invitation_token])
+    if @invitations
+      logger.debug "(new) #{@invitations.email}"
+      session[:signup_params]['email'] = @invitations.email 
+      session[:signup_params]['first_name'] = @invitations.first_name 
+      session[:signup_params]['last_name'] = @invitations.last_name 
+    end
+  end
+  
+  def create
+    logger.debug "(create) #{params[:user]}"    
+    session[:signup_params].deep_merge!(params[:user]) if params[:user]
+    @user = User.new(session[:signup_params])
+    @user.current_step = session[:signup_step]
+    
+    if params[:back_button]
+      @user.previous_step
+    elsif @user.last_step?
+      @user.save
+    else
+      @user.next_step
+    end
+    
+    session[:signup_step] = @user.current_step 
+
+    if @user.new_record?
+      render "new"
+    else
+      session[:signup_params] = session[:signup_step] = nil
+      #flash[:notice] = "User created!"
+      cookies[:auth_token] = @user.auth_token
+      redirect_to signup_success_path
+    end  
+  end
+  
+  def success
+  end
+  
+end
