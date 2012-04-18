6873991ed8c0cab96cdf712ef2bde01e264ac7d3
Michael <mwu@caregaroo.com>
2012-04-17 22:42:24 -0700

    cleanup css and js in application layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,4 +9,21 @@
 //= require jquery-ui
 // disable require_tree .
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-23336907-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+try{
+    function yt_callback(category,action,label,value){
+    	_gaq.push(['_trackEvent',category,action,label,value]);
+    }
+    var ytTracker = new YoutubeTracker(false, yt_callback, true);
+} catch(e){}
+
 
