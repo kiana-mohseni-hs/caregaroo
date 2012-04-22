9eefcfc7c3358216a64ac07a0873dde78c3afcac
Michael Wu <mwu@caregaroo.com>
2012-04-21 18:59:09 -0700

    add top nav username drop down

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,8 +39,31 @@ jQuery(document).ready(function() {
 		overlayOpacity:0.7,
 		titleShow		: false
 	});
+	
 	$("a.news_comment").click(function(){
 		var id = $(this).attr("newsid");
 		$("#new_comment_section_"+id).show();
 	});
+	
+	$(".dropdown dt a").click(function() {
+	    $(".dropdown dd ul").toggle();
+	});
+           
+	$(".dropdown dd ul li a").click(function() {
+	    var text = $(this).html();
+	    $(".dropdown dt a span").html(text);
+	    $(".dropdown dd ul").hide();
+	    $("#result").html("Selected value is: " + getSelectedValue("sample"));
+	});
+           
+	function getSelectedValue(id) {
+	    return $("#" + id).find("dt a span.value").html();
+	}
+
+	$(document).bind('click', function(e) {
+	    var $clicked = $(e.target);
+	    if (! $clicked.parents().hasClass("dropdown"))
+	        $(".dropdown dd ul").hide();
+	});
+
 });
