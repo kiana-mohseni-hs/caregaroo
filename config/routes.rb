2ab27edaeb23c03a7c1a3ef31de1ff72dd4b5040
Michael <mwu@caregaroo.com>
2012-04-24 00:00:52 -0700

    fixed profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,17 @@
 Auth::Application.routes.draw do
 
   match '/calendar(/:year(/:month))' => 'calendar#index', :as => :calendar, :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
-
   get "calendar/show"
 
-  # pilot app
+  # app routes
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#login", :as => "login"
   get "register" => "register#index", :as => "register"
   post "register" => "register#create", :as => "register"
-  get "profile" => "profile#index", :as => "profile"
-  post "profile/basic" => "profile#update_basic", :as => "update_basic_profile"
-  get "profile/info" => "profile#info", :as => "info_profile"
-  get "profile/:user_id/info" => "profile#info", :as => "user_info_profile"
-  get "profile/info/edit" => "profile#edit_info", :as => "edit_info_profile"
-  post "profile/info" => "profile#update_info", :as => "update_info_profile"
-  get "profile/password" => "profile#password", :as => "password_profile"
-  post "profile/password" => "profile#update_password", :as => "update_password_profile"
-  get "profile/notifications" => "profile#notifications", :as => "notifications_profile"
+  get "profile" => "profile#info", :as => "profile"
+  get "profile/edit" => "profile#edit_info", :as => "edit_info_profile"
+  get "profile/:user_id" => "profile#info", :as => "user_info_profile"
+  post "profile" => "profile#update_info", :as => "update_info_profile"
   get "news" => "posts#index", :as => "news"
   get "members" => "members#index", :as => "members"
   get "invite" => "invitations#index", :as => "invitation"
@@ -28,25 +22,20 @@ Auth::Application.routes.draw do
   post "signup/create" => "signup#create", :as => "signup_create"
   get "signup/success" => "signup#success", :as => "signup_success"
   get "signup/:invitation_token" => "signup#new", :as => "signup_with_token"
-#  get "pilot" => "invitations#new", :as => "pilot"
-#  get "pilot/success" => "invitations#success", :as => "pilot_success"
-#  post "pilotsignup" => "pilot_signups#signup", :as => "pilot_signup" 
 
-  # pilot www homepage 
+  # marketing routes
   post "pilot_signups/:id" => "pilot_signups#destroy", :as => "signup_delete"
   get "download/ebook" => "download#ebook", :as => "download_ebook"
   get "download/faq" => "download#faq", :as => "download_faq"
   get "admin" => "pilot_signups#index", :as => "admin"
   post "signup" => "register#signup", :as => "signup"
   match "product" => "home#product"
+  
   root :to => "home#index", :as => "root"
   
-  # resources :users
   resources :sessions
   resources :password_resets
-  # resources :invitations
   resources :messages
-  # resources :profiles
   resources :posts
   resources :comments
   resources :pilot_signups
