9eab2efceea4ec91e174a04bb82af36190980ab7
Michael <mwu@caregaroo.com>
2012-04-29 21:57:50 -0700

    intercept mail when env == dev

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,26 +1,3 @@
-=begin
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => "587",
-  :domain               => "caregaroo.com",
-  :user_name            => "app@caregaroo.com",
-  :password             => "4MdRA+3[",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+require 'development_mail_interceptor'
+ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
 
-email_settings = YAML::load(File.open("#{Rails.root.to_s}/config/email.yml"))
-ActionMailer::Base.smtp_settings = email_settings[Rails.env] unless email_settings[Rails.env].nil?
-
-# port 465, 587
-if Rails.env.development?
-  ActionMailer::Base.default_url_options[:host] = "localhost:3000"
-elsif Rails.env.test?
-  ActionMailer::Base.default_url_options[:host] = "test.caregaroo.com"
-else
-  ActionMailer::Base.default_url_options[:host] = "caregaroo.com"
-end
-#require 'development_mail_interceptor'
-#ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
-
-=end
