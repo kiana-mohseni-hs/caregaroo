45263fdc1d3d56c7f440dcb3402bd0d595d11a3f
Michael <mwu@caregaroo.com>
2012-04-29 21:16:09 -0700

    add email.yml file for different environments

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,3 +1,4 @@
+=begin
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => "587",
@@ -7,6 +8,11 @@ ActionMailer::Base.smtp_settings = {
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
+=end
+
+email_settings = YAML::load(File.open("#{Rails.root.to_s}/config/email.yml"))
+ActionMailer::Base.smtp_settings = email_settings[Rails.env] unless email_settings[Rails.env].nil?
+
 # port 465, 587
 if Rails.env.development?
   ActionMailer::Base.default_url_options[:host] = "localhost:3000"
@@ -15,5 +21,5 @@ elsif Rails.env.test?
 else
   ActionMailer::Base.default_url_options[:host] = "caregaroo.com"
 end
-require 'development_mail_interceptor'
-ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
+#require 'development_mail_interceptor'
+#ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
