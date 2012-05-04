628dd9bea018945cf9f4a3685c0e784b84a2eef1
Michael <mwu@caregaroo.com>
2012-05-04 01:06:00 -0700

    add and remove comments using ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,58 +1,38 @@
 class CommentsController < ApplicationController  
   before_filter :require_user
   
-  # GET /comments
-  # GET /comments.json
   def index
     @comments = Comment.where("post_id = ? and created_at > ?", params[:post_id], Time.at(params[:after].to_i + 1))
   end
 
-=begin
-  # GET /comments/1
-  # GET /comments/1.json
-  def show
-    @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render :json => @comment }
-    end
-  end
-
-  # GET /comments/new
-  # GET /comments/new.json
-  def new
-    @comment = Comment.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @comment }
-    end
-  end
-=end
-
-  # GET /comments/1/edit
   def edit
     @comment = Comment.find(params[:id])
   end
 
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = Comment.new(params[:comment])
     @comment.user_id = @current_user.id
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to news_path, :notice => 'Comment was successfully created.' }
-        format.json { render :json => @comment, :status => :created, :location => @comment }
-      # else
-        # format.html { render :action => "new" }
-        # format.json { render :json => @comment.errors, :status => :unprocessable_entity }
+        @comments = Comment.where("post_id = ?", @comment.post_id)
+        format.html { redirect_to news_path }
+        format.js
       end
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comments = Comment.where("post_id = ?", @comment.post_id)
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to news_url }
+      format.js
+    end
+  end
+  
 =begin
   # PUT /comments/1
   # PUT /comments/1.json
@@ -70,16 +50,4 @@ class CommentsController < ApplicationController
     end
   end
 =end  
-
-  # DELETE /comments/1
-  # DELETE /comments/1.json
-  def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to news_url }
-      format.json { head :ok }
-    end
-  end
 end
