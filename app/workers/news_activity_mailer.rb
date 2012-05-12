2cc319f16d4f6c9c8105d1bf50ca034c4820c13d
Michael <mwu@caregaroo.com>
2012-05-12 00:31:07 -0700

    update news activity mailer

--- app/workers/news_activity_mailer.rb
+++ app/workers/news_activity_mailer.rb
@@ -3,7 +3,7 @@ class NewsActivityMailer
   def self.perform(post_id)
     post = Post.find(post_id)
     network = Network.find(post.network_id)
-    members = User.joins(:notification).where("users.network_id = ?", post.network_id)
+    members = User.joins(:notification).where("users.network_id = ? and notifications.post_update = ?", post.network_id, true)
     # emails = members.collect(&:email).join(";")
     members.each do |m|
       UserMailer.news_activity(post, m, network.network_for_who).deliver
