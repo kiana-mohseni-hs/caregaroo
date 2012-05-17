081a7986f1a7647199348ebe1e854e79d67a3055
Michael <mwu@caregaroo.com>
2012-05-16 23:29:01 -0700

    update avatar images for post and comments

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -27,7 +27,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
 #     "/images/avatar/" + [version_name, "default.png"].compact.join('_')
-     "/assets/avatar/photo_place_holder.png"
+     "/assets/photo_place_holder.png"
   end
 
   # Process files as they are uploaded:
