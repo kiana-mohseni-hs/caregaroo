6bdc0686500bb311f9d9b71e9c98ebbf0571a2ba
dazl <davide.zilioli@gmail.com>
2012-09-04 16:38:49 -0700

    add precompile asset

--- config/environments/heroku.rb
+++ config/environments/heroku.rb
@@ -47,7 +47,7 @@ Cg2App::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['marketing.css']
+  config.assets.precompile += %w( marketing.css bootstrap.css )
   
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
