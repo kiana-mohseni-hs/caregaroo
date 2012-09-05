12e880e069f410f609643ef171dcfcffe1eb9af4
dazl <davide.zilioli@gmail.com>
2012-09-04 18:02:23 -0700

    precompile assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,6 +8,10 @@
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require jquery-ui-timepicker.js
+//= require jquery.fancybox-1.3.4.pack.js
+//= require jquery.purr.js
+//= require jquery.hint-with-password.js
+//= require jquery.autosize-min.js
 //= require_tree ./app
 
 var _gaq = _gaq || [];
