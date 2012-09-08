003f697fa6e9526268afbf594b266b5e14481a23
dazl <davide.zilioli@gmail.com>
2012-09-08 16:47:04 -0700

    close #38

--- app/models/event.rb
+++ app/models/event.rb
@@ -38,6 +38,10 @@ class Event < ActiveRecord::Base
   scope :visible, where(canceled: false)
   scope :future, where(['start_at >= ?', Time.now.beginning_of_day])
   
+  def is_new?
+    self.updated_at === self.created_at
+  end
+  
   def cancel(canceler_id)
     update_attributes(canceled_by_id: canceler_id, canceled: true)
   end
@@ -60,30 +64,18 @@ class Event < ActiveRecord::Base
   
   def post_new_event
     create_post(
-      name: "New Event: #{name}", 
-      content: "New Event: #{name} (#{event_type.name})<br/>" << 
-               datetimestring <<
-               locationstring,
+      name: name, 
+      content: name,
       user_id: created_by_id, 
       network_id: network_id 
       )
     update_column("post_id", post.id)
   end
-    
+        
   def update_post
-    change = canceled ? "Canceled" : "Updated"
-    eventtype = event_type.nil? ? "" : event_type.name
-    new_content = "#{change} Event: #{name} (#{eventtype})<br/> " << 
-                  datetimestring <<
-                  locationstring
-    if canceled
-      new_content << "<br/>canceled by #{canceler.first_name}"
-    else
-      new_content << "<br/>updated by #{updater.first_name}" unless updater == creator
-    end
     post.update_attributes(
-      name: "#{change} Event: #{name}", 
-      content: new_content
+      name: name, 
+      content: name
       ) unless post.nil?
   end
   
