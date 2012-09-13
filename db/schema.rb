1117c4d6016948b6dd1b0b51bb6368d1f3457159
dazl <davide.zilioli@gmail.com>
2012-09-13 16:40:41 -0700

    run migration add network_id to comment

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(:version => 20120907214027) do
     t.integer  "network_id"
     t.integer  "event_type_id",                  :default => 1
     t.string   "location"
-    t.text     "description"
+    t.text     "description",    :limit => 1275
     t.integer  "created_by_id"
     t.integer  "updated_by_id"
     t.integer  "post_id"
