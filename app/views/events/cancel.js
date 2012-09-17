1104426723a8def2cf1fa688fea4ec4d2be5c5be
dazl <davide.zilioli@gmail.com>
2012-09-17 12:09:53 -0700

    back to coffee since the switch to js did not help

--- app/views/events/cancel.js
+++
@@ -1,6 +0,0 @@
-$('#<%= dom_id(@event) %>').fadeOut(function() {
-  if ($(this).prev().hasClass("events_list_date_row") && $(this).next().hasClass("events_list_date_row")) {
-    $(this).prev().remove();
-  }
-  return $(this).remove();
-});
