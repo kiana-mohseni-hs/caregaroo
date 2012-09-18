0f2b5f5407659701687780cb7eb2430518b6329a
Michael Wu <mikewuch@gmail.com>
2012-09-17 23:31:41 -0700

    disable rescue when creating comments

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -64,32 +64,5 @@ class RegisterController < ApplicationController
   def success
       render "success", :layout => "app_no_nav"
   end
-    
-=begin  // wizard step 
-    session[:signup_params].deep_merge!(params[:user]) if params[:user]
-    @user = User.new(session[:signup_params])
-    @user.current_step = session[:signup_step]
-    
-    if params[:back_button]
-      @user.previous_step
-    elsif @user.last_step?
-      @user.save
-    else
-      @user.next_step
-    end
-    
-    session[:signup_step] = @user.current_step 
-
-
-    if @user.new_record?
-      render "success"
-    else
-      session[:signup_params] = session[:signup_step] = nil
-      #flash[:notice] = "User created!"
-      cookies[:auth_token] = @user.auth_token
-      #redirect_to register_success_path
-      render "success"
-#    end  
-=end
   
 end
