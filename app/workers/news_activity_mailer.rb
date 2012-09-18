3ca86197cff86cf46955faeb348d7a8a2cb4bc54
Michael Wu <mikewuch@gmail.com>
2012-09-18 08:15:15 -0700

    turn off autocomplete fixed news activities to get invitation emails first then members emails

--- app/workers/news_activity_mailer.rb
+++ app/workers/news_activity_mailer.rb
@@ -6,13 +6,13 @@ class NewsActivityMailer
     members = User.joins(:notification).where("users.network_id = ? and notifications.post_update = ?", post.network_id, true)
     # emails = members.collect(&:email).join(";")
     user_list = {}
-    members.each do |m|
-      # UserMailer.news_activity(post, m, network.network_for_who).deliver
-      user_list[m.email] = {:first_name => m.first_name, :last_name => m.last_name}
-    end
     invitations = Invitation.where("network_id = ?", post.network_id)
     invitations.each do |m|
         user_list[m.email] = {:first_name => m.first_name, :last_name => m.last_name, :token => m.token}
+    end
+    members.each do |m|
+      # UserMailer.news_activity(post, m, network.network_for_who).deliver
+      user_list[m.email] = {:first_name => m.first_name, :last_name => m.last_name}
     end
     user_list.each do |email, u|
       UserMailer.news_activity(post, email, u[:first_name], u[:last_name], network.network_for_who, u[:token]).deliver
