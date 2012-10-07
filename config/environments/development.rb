f6fe5e6aba203927305cb11c39094dda7cb299a0
Michael Wu <mikewuch@gmail.com>
2012-10-06 17:27:54 -0700

    remove redisToGo config from production and development.rb files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,6 @@ Cg2App::Application.configure do
     config.storage = :file
   end
   
-  ENV["REDISTOGO_URL"] = 'redis://localhost:6379'
+  # ENV["REDISTOGO_URL"] = 'redis://localhost:6379'
   
 end
