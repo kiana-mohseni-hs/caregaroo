97a70b3a217109837f8de8f48d4d79b56606496d
Michael Wu <mikewuch@gmail.com>
2012-10-13 18:05:23 -0700

    remote limit from the migration script not working heroku

--- db/migrate/20121014003548_change_posts_content.rb
+++ db/migrate/20121014003548_change_posts_content.rb
@@ -1,7 +1,7 @@
 class ChangePostsContent < ActiveRecord::Migration
   def up
-    change_column :posts, :content, :text, :limit => 3000
-    change_column :comments, :content, :text, :limit => 3000
+    change_column :posts, :content, :text
+    change_column :comments, :content, :text
   end
 
   def down
