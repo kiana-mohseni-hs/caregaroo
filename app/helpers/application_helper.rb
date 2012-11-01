b22dd5df98862cf326fa422c6b7119a1cacb87a6
dazl <davide.zilioli@gmail.com>
2012-10-31 21:27:28 -0700

    fix CK-82

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def relationship_in_parenthesis(user, network_id)
+    if user.present? and network_id.present? and user.network_relationship(network_id).present?
+      "(" + user.network_relationship(network_id) + ")"
+    else
+      ""
+    end
+  end
+  
 end
