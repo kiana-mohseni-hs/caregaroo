8c07fe93b34e1133e0ad5a64f3a90bf38ff56dd2
FabianoPS <fabianosoriani@gmail.com>
2012-11-25 13:56:31 -0800

    quick migration fix

--- db/migrate/20121120183842_add_counter_cache_to_networks.rb
+++ db/migrate/20121120183842_add_counter_cache_to_networks.rb
@@ -4,7 +4,7 @@ class AddCounterCacheToNetworks < ActiveRecord::Migration
     add_column :networks, :posts_count, :integer, null: false, default: 0
     add_column :networks, :events_count, :integer, null: false, default: 0
     
-    # Network.reset_column_information
+    Network.reset_column_information
 
     Network.all.each do |n|
     	Network.update_counters n.id, :users_count  => n.users.length, :posts_count  => n.posts.length, :events_count => n.events.length
