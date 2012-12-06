05f161e3506f86ef2f0a8f303baa95733c1d1285
Julien Escueta <julien.escueta+github.com@gmail.com>
2012-12-06 03:41:00 -0800

    exit gracefully if networks.json does not exist

--- test/uitest/tests/suites/pre/loadtestnetworksfromdisk.js
+++ test/uitest/tests/suites/pre/loadtestnetworksfromdisk.js
@@ -1,9 +1,17 @@
 casper.start();
 
-casper.then(function () {
-  var json = require('fs').read('tests/suites/pre/networks.json');
-  casper.caregaroo.fixtures = {};
-  casper.caregaroo.fixtures.networks = JSON.parse(json);
+casper.then(function loadTestNetworksFromDisk() {
+  var fs = require('fs');
+  var pathtonetworksjson = 'tests/suites/pre/networks.json';
+
+  if (fs.exists(pathtonetworksjson)) {
+    var json = fs.read(pathtonetworksjson);
+    casper.caregaroo.fixtures = {};
+    casper.caregaroo.fixtures.networks = JSON.parse(json);
+  } else {
+    casper.echo('### ERROR: Unable to find ' + pathtonetworksjson);
+    casper.exit(1);
+  }
 });
 
 casper.run(function () {
