d3b9af3324dde0b5cde82afadfae79506bf6236a
Julien Escueta <julien.escueta+github.com@gmail.com>
2012-12-06 03:31:14 -0800

    Load network data from disk
    We write the networks data (and users) to disk every time
    generatetestnetworks.js is invoked. The default behaviour is to generate
    the networks data.
    
    Generating networks data is complex and time consuming, so for the users
    willing to use old data, they can specify -l in the shell script in
    order to load networks data from disk.

--- test/uitest/tests/suites/pre/loadtestnetworksfromdisk.js
+++ test/uitest/tests/suites/pre/loadtestnetworksfromdisk.js
@@ -1,12 +1,9 @@
 casper.start();
 
 casper.then(function () {
-  var dump, networks;
-  dump = require('fs').read('tests/suites/pre/testdata.json');
-  networks = JSON.parse(dump);
-  console.log('networks.network1.networkInfo.name: ' + networks.network1.networkInfo.name);
-
-  casper.test.assertEquals(networks.network1.networkInfo.name, 'Lalalal', 'Verify that we can parse JSON from disk');
+  var json = require('fs').read('tests/suites/pre/networks.json');
+  casper.caregaroo.fixtures = {};
+  casper.caregaroo.fixtures.networks = JSON.parse(json);
 });
 
 casper.run(function () {
